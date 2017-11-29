4506f60422839dff6d931717f8923e7a4a00d0cb
Christopher Rocha <christopherrocha@Christophers-MacBook-Pro.local>
2017-11-28 20:34:49 -0600

    first commit. created server.js

---
+++ server.js
@@ -0,0 +1,49 @@
+//dependencies
+// var express = require("express");
+// var methodOverride = require("method-override");
+// var bodyParser = require("body-parser");
+// var logger = require("morgan");
+// var mongoose = require("mongoose");
+import "express";
+import * as methodOverride from "method-override";
+import * as bodyParser from "body-parser";
+import * as logger from "morgan";
+import "mongoose";
+
+mongoose.Promise = Promise;
+
+// initialize express
+var app = express();
+
+app.set('port', process.env.PORT || 8080);
+
+// use morgan and body parser
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+var routes = require("./controllers/routes.js")
+app.use("/", routes);
+
+// var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+//                 replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };       
+ 
+//var mongodbUri = 'mongodb://lernt:burnunit@ds133814.mlab.com:33814/lernt';
+ 
+// mongoose.connect(mongodbUri, options);
+// var db = mongoose.connection;             
+ 
+db.on('error', console.error.bind(console, 'connection error:'));  
+ 
+db.once('open', function() {
+  // Wait for the database connection to establish, then start the app.
+  console.log('Connected to '+ mongodbUri);                         
+});
+
+server.listen(app.get('port'), function() {
+  console.log("App running!");
+});
